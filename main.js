208dcec08e94975a0a0dc82f9646a222b20aa790
Rohan kumar <75016222+Rudraksh42@users.noreply.github.com>
2022-02-04 09:11:48 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+
+function setup() {
+    canvas = createCanvas(600, 300);
+    canvas.center();
+    background("white");
+}
+function clearCanvas() {
+    background("white");
+}
+timer_counter = 0;
